6b25b0eaa2439204d697fa3e764d47b07720a441
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 14:06:48 -0300

    Update skill.module.ts

--- server/src/skill/skill.module.ts
+++ server/src/skill/skill.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { SkillService } from './skill.service';
 import { SkillController } from './skill.controller';
+import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
+import { ClubEntity } from 'src/club/entities/club.entity';
+import { MatchEntity } from 'src/match/entities/match.entity';
+import { PositionEntity } from 'src/position/entities/position.entity';
+import { SkillEntity } from './entities/skill.entity';
+import { SportEntity } from 'src/sport/entities/sport.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SportmanEntity,
+      ClubEntity,
+      MatchEntity,
+      PositionEntity,
+      SkillEntity,
+      SportEntity
+    ])
+  ],
   controllers: [SkillController],
-  providers: [SkillService],
+  providers: [SkillService]
 })
 export class SkillModule {}
