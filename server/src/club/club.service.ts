2a016c9ac4c405e4cefa0bef3fee77b5f94bbc27
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 14:06:07 -0300

    Update club.service.ts

--- server/src/club/club.service.ts
+++ server/src/club/club.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateClubDto } from './dto/create-club.dto';
 import { UpdateClubDto } from './dto/update-club.dto';
+import { ClubEntity } from './entities/club.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ClubService {
-  create(createClubDto: CreateClubDto) {
-    return 'This action adds a new club';
+  constructor(
+    @InjectRepository(ClubEntity)
+    private readonly clubRepository: Repository<ClubEntity>
+  ) {}
+
+  /**
+   * Método para crear un nuevo los club
+   * @param {CreateUserDto} createUserDto - Los datos del club a crear
+   */
+  public async create(createClubDto: CreateClubDto) {
+    const newClub = await this.clubRepository.save(createClubDto);
+    if (!newClub) {
+      throw new HttpException('the new club is not created', 501);
+    }
+    return newClub;
+  }
+  /**
+   * Método para obtener todos los clubes
+   */
+  public async findAll() {
+    return await this.clubRepository.find({ where: { isDelete: false } });
   }
 
-  findAll() {
-    return `This action returns all club`;
+  /**
+   * Método para obtener un club por su ID
+   * @param {string} id - El ID del club a buscar
+   */
+  public async findOne(id: string) {
+    const club = await this.clubRepository
+      .createQueryBuilder('club')
+      .where({ id })
+      .getOne();
+
+    // Si no se encuentra el club, lanzar una excepción
+    if (!club) throw new HttpException(`Club con ID ${id} no encontrado`, 404);
+
+    // Devolver el club encontrado
+    return club;
+  }
+  /**
+   * Método para actualizar un club por su ID
+   * @param {string} id - El ID del club a actualizar
+   * @param {UpdateClubDto} updateClubDto - Los datos del club a actualizar
+   */
+  public async update(id: string, updateClubDto: UpdateClubDto) {
+    const club = await this.findOne(id);
+    if (!club) {
+      throw new HttpException(`Club con id ${id} no encontrado`, 404);
+    }
+    for (const key in updateClubDto) {
+      club[key] = updateClubDto[key];
+    }
+    return await this.clubRepository.save(club);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} club`;
-  }
-
-  update(id: number, updateClubDto: UpdateClubDto) {
-    return `This action updates a #${id} club`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} club`;
+  public async remove(id: string) {
+    await this.clubRepository.update(id, { isDelete: true });
+    return await this.findOne(id);
   }
 }
