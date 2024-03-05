d0e046bf85f1860a93daef0ba5d4b3534c02dc0b
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 12:23:41 -0300

    Update skill.entity.ts

--- server/src/skill/entities/skill.entity.ts
+++ server/src/skill/entities/skill.entity.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from 'src/config/base.entity';
+import { SportEntity } from 'src/sport/entities/sport.entity';
 import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
 import { OneToOne } from 'typeorm';
 
 export class SkillEntity extends BaseEntity {
   @OneToOne(() => SportmanEntity, (sportman) => sportman.skill)
   sportman: SportmanEntity;
+
+  @OneToOne(() => SportEntity, (sport) => sport.skill)
+  sport: SportEntity;
 }
