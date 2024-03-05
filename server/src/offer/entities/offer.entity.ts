3e1f569bcca4e02b963003b54dc11d1cc4adb379
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 12:23:27 -0300

    Update offer.entity.ts

--- server/src/offer/entities/offer.entity.ts
+++ server/src/offer/entities/offer.entity.ts
@@ -1,8 +1,17 @@
 import { ClubEntity } from 'src/club/entities/club.entity';
 import { BaseEntity } from 'src/config/base.entity';
-import { ManyToOne } from 'typeorm';
+import { MatchEntity } from 'src/match/entities/match.entity';
+import { PositionEntity } from 'src/position/entities/position.entity';
+import { JoinColumn, ManyToOne, OneToMany, OneToOne } from 'typeorm';
 
 export class OfferEntity extends BaseEntity {
+  @OneToOne(() => PositionEntity)
+  @JoinColumn()
+  position: PositionEntity;
+
+  @OneToMany(() => MatchEntity, (match) => match.offer)
+  match: MatchEntity[];
+
   @ManyToOne(() => ClubEntity, (club) => club.offers, { nullable: true })
   club?: ClubEntity;
 }
