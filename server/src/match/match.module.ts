e15bef8e4330a03e1d4e51b37710f8e0e5617c76
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 14:06:32 -0300

    Update match.module.ts

--- server/src/match/match.module.ts
+++ server/src/match/match.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
+import { ClubEntity } from 'src/club/entities/club.entity';
+import { MatchEntity } from './entities/match.entity';
+import { PositionEntity } from 'src/position/entities/position.entity';
+import { SkillEntity } from 'src/skill/entities/skill.entity';
+import { SportEntity } from 'src/sport/entities/sport.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SportmanEntity,
+      ClubEntity,
+      MatchEntity,
+      PositionEntity,
+      SkillEntity,
+      SportEntity,
+      UserEntity
+    ])
+  ],
   controllers: [MatchController],
-  providers: [MatchService],
+  providers: [MatchService]
 })
 export class MatchModule {}
