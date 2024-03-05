1ae72caaa35c3ab1f08bbd2b77d61030130cb39d
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 12:23:20 -0300

    Update match.entity.ts

--- server/src/match/entities/match.entity.ts
+++ server/src/match/entities/match.entity.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from 'src/config/base.entity';
+import { OfferEntity } from 'src/offer/entities/offer.entity';
 import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
-import { ManyToMany } from 'typeorm';
+import { ManyToMany, ManyToOne } from 'typeorm';
 
 export class MatchEntity extends BaseEntity {
   @ManyToMany(() => SportmanEntity)
   sportmen: SportmanEntity[];
+
+  @ManyToOne(() => OfferEntity, (offer) => offer.match)
+  offer: OfferEntity;
 }
