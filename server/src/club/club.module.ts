36758dd0288c006ef3c72c7a4426861698a94d78
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 14:06:05 -0300

    Update club.module.ts

--- server/src/club/club.module.ts
+++ server/src/club/club.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ClubService } from './club.service';
 import { ClubController } from './club.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { OfferEntity } from 'src/offer/entities/offer.entity';
+import { PositionEntity } from 'src/position/entities/position.entity';
+import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
+import { ClubEntity } from './entities/club.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      ClubEntity,
+      OfferEntity,
+      PositionEntity,
+      SportmanEntity
+    ])
+  ],
+  exports: [],
   controllers: [ClubController],
-  providers: [ClubService],
+  providers: [ClubService]
 })
 export class ClubModule {}
