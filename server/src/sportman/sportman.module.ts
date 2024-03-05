2d26234fdf302732c3398470c8c74764f5ce7d4b
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-05 14:07:05 -0300

    Update sportman.module.ts

--- server/src/sportman/sportman.module.ts
+++ server/src/sportman/sportman.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { SportmanService } from './sportman.service';
 import { SportmanController } from './sportman.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SportmanEntity } from './entities/sportman.entity';
+import { ClubEntity } from 'src/club/entities/club.entity';
+import { MatchEntity } from 'src/match/entities/match.entity';
+import { PositionEntity } from 'src/position/entities/position.entity';
+import { SkillEntity } from 'src/skill/entities/skill.entity';
+import { SportEntity } from 'src/sport/entities/sport.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SportmanEntity,
+      ClubEntity,
+      MatchEntity,
+      PositionEntity,
+      SkillEntity,
+      SportEntity
+    ])
+  ],
+  exports: [],
   controllers: [SportmanController],
-  providers: [SportmanService],
+  providers: [SportmanService]
 })
 export class SportmanModule {}
