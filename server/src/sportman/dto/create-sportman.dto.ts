acf2015f3d74cd3bd5241403dabf8b7c3ac4b233
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-06 14:28:54 -0300

    Update create-sportman.dto.ts

--- server/src/sportman/dto/create-sportman.dto.ts
+++ server/src/sportman/dto/create-sportman.dto.ts
@@ -1,1 +1,31 @@
-export class CreateSportmanDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested
+} from 'class-validator';
+
+enum SportmanType {
+  player = 'player',
+  coach = 'coach'
+}
+class SportmanData {
+  @IsEnum(SportmanType)
+  @IsNotEmpty()
+  type: SportmanType;
+
+  @IsObject()
+  @IsOptional()
+  info: { [key: string]: any };
+}
+
+export class CreateSportmanDto {
+  @ValidateNested() // Validar la propiedad anidada
+  sportmanData: SportmanData;
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+}
