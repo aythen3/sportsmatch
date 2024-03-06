552e9a2ffb863d9ffccd48202d519841d2f9a147
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-06 14:28:51 -0300

    Update sport.service.ts

--- server/src/sport/sport.service.ts
+++ server/src/sport/sport.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateSportDto } from './dto/create-sport.dto';
 import { UpdateSportDto } from './dto/update-sport.dto';
+import { SportEntity } from './entities/sport.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DEPORTES } from './utils/sportAutoCreate';
 
 @Injectable()
 export class SportService {
-  create(createSportDto: CreateSportDto) {
-    return 'This action adds a new sport';
+  constructor(
+    @InjectRepository(SportEntity)
+    private readonly sportRepository: Repository<SportEntity>
+  ) {}
+
+  public async create(createSportDto: CreateSportDto) {
+    const newSport = await this.sportRepository.save(createSportDto);
+
+    // Si no se pudo crear el nuevo perfil, lanzar una excepción
+    if (!newSport) {
+      throw new HttpException('The new sport is not created', 501);
+    }
+
+    // Devolver el nuevo perfil del usuario
+    return newSport;
   }
 
-  findAll() {
-    return `This action returns all sport`;
+  public async findAll() {
+    return this.sportRepository.find({ where: { isDelete: false } });
   }
 
-  findOne(id: number) {
+  public async findOne(id: number) {
     return `This action returns a #${id} sport`;
   }
 
-  update(id: number, updateSportDto: UpdateSportDto) {
-    return `This action updates a #${id} sport`;
+  public async update(id: number, updateSportDto: UpdateSportDto) {
+    return `This action updates a #${updateSportDto} sport`;
   }
 
-  remove(id: number) {
+  public async remove(id: number) {
     return `This action removes a #${id} sport`;
+  }
+
+  public async sportMaker() {
+    const deportes = DEPORTES;
+    try {
+      const createdSport = await this.sportRepository.save(deportes);
+      return createdSport;
+    } catch (error) {
+      throw new HttpException('Error creating sportmen', 505);
+    }
   }
 }
