c5d611e0073ec678f65be9f6ead290138407fc26
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-07 11:17:49 -0300

    Update update-sportman.dto.ts

--- server/src/sportman/dto/update-sportman.dto.ts
+++ server/src/sportman/dto/update-sportman.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSportmanDto } from './create-sportman.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateSportmanDto extends PartialType(CreateSportmanDto) {}
+export class UpdateSportmanDto extends PartialType(CreateSportmanDto) {
+  @IsBoolean()
+  @IsOptional()
+  isDelete: boolean;
+}
