a59c4d39550c1e760bab4c9bf088720f60a85142
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-07 11:17:53 -0300

    Update update-user.dto.ts

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsBoolean()
+  @IsOptional()
+  isDelete: boolean;
+}
