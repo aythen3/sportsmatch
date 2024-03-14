8102559998f83db5d95a5633223dff338553a84b
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-14 10:57:58 -0300

    Update create-post.dto.ts

--- server/src/post/dto/create-post.dto.ts
+++ server/src/post/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  authorType: string;
+}
