2098e9175eeb8412ca67b1a105bf1348d1c26779
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-14 10:57:55 -0300

    Update update-like.dto.ts

--- server/src/like/dto/update-like.dto.ts
+++ server/src/like/dto/update-like.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLikeDto } from './create-like.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateLikeDto extends PartialType(CreateLikeDto) {}
+export class UpdateLikeDto extends PartialType(CreateLikeDto) {
+  @IsBoolean()
+  @IsOptional()
+  isDelete: boolean;
+}
