3e4ce76963a731954a64459ae179ac9fa81d4900
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-14 09:25:27 -0300

    Update user.module.ts

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -7,9 +7,21 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthJwtService } from 'src/auth-jwt/auth-jwt.service';
 import { ClubEntity } from 'src/club/entities/club.entity';
 import { SportEntity } from 'src/sport/entities/sport.entity';
+import { PostEntity } from 'src/post/entities/post.entity';
+import { CommentEntity } from 'src/comment/entities/comment.entity';
+import { LikeEntity } from 'src/like/entities/like.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, ClubEntity, SportEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      ClubEntity,
+      SportEntity,
+      PostEntity,
+      CommentEntity,
+      LikeEntity
+    ])
+  ],
   exports: [UserService],
   controllers: [UserController],
   providers: [UserService, JwtService, AuthJwtService]
