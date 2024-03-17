92571f2b4a2652e38ffdbd4faa1555a096404bc1
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-17 16:46:15 -0300

    Update comment.service.ts

--- server/src/comment/comment.service.ts
+++ server/src/comment/comment.service.ts
@@ -4,15 +4,28 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 import { CommentEntity } from './entities/comment.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UserService } from 'src/user/user.service';
+import { PostService } from 'src/post/post.service';
 
 @Injectable()
 export class CommentService {
   constructor(
     @InjectRepository(CommentEntity)
-    private readonly commentRepository: Repository<CommentEntity>
+    private readonly commentRepository: Repository<CommentEntity>,
+    private readonly userService: UserService,
+    private readonly postService: PostService
   ) {}
   public async create(createCommentDto: CreateCommentDto) {
-    const comment = await this.commentRepository.save(createCommentDto);
+    console.log(createCommentDto);
+    const author = await this.userService.findOne(createCommentDto.author);
+
+    const post = await this.postService.findOne(createCommentDto.post);
+
+    const comment = new CommentEntity();
+    comment.content = createCommentDto.content;
+    comment.author = author;
+    comment.post = post;
+    await this.commentRepository.save(comment);
     // Si no se pudo crear el nuevo comment, lanzar una excepción
     if (!comment) {
       throw new HttpException('The new comment is not created', 501);
@@ -23,6 +36,37 @@ export class CommentService {
 
   public async findAll() {
     return this.commentRepository.find({ where: { isDelete: false } });
+  }
+
+  public async findAllByPost(postId: string): Promise<CommentEntity[]> {
+    // Buscar todos los comentarios asociados al post especificado
+    const comments = await this.commentRepository.find({
+      where: { post: { id: postId } },
+      relations: ['author'] // Incluir información del autor del comentario
+    });
+    // Si no se encuentran comentarios, lanzar una excepción
+    if (!comments || comments.length === 0) {
+      throw new HttpException(
+        `No comments found for post with ID ${postId}`,
+        404
+      );
+    }
+    // Devolver los comentarios encontrados
+    return comments;
+  }
+
+  public async findAllByUser(id: string): Promise<CommentEntity[]> {
+    // Buscar todos los comentarios asociados al post especificado
+    const comments = await this.commentRepository.find({
+      where: { author: { id: id } },
+      relations: ['post'] // Incluir información del autor del comentario
+    });
+    // Si no se encuentran comentarios, lanzar una excepción
+    if (!comments || comments.length === 0) {
+      throw new HttpException(`No comments found for post with ID ${id}`, 404);
+    }
+    // Devolver los comentarios encontrados
+    return comments;
   }
 
   public async findOne(id: string) {
