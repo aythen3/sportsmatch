9ae89cd0dc1a753c33954d4e4f09458fc7d5c881
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-20 11:44:33 -0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { ValidationPipe } from '@nestjs/common';
 import { CORS } from './config/cors';
-import { WsAdapter } from '@nestjs/platform-ws'; // Importa el adaptador de WebSocket
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,8 +22,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
-
-  app.useWebSocketAdapter(new WsAdapter(app)); // Usa el adaptador de WebSocket
 
   await app.listen(+configService.get('PORT') || 3000);
   console.log(`Api running on: localhost:${configService.get('PORT') || 3000}`);
