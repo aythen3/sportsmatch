bd4dbd14f2ad12c16ca47fdc94a2c963344b0e1c
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-28 09:17:14 -0300

    Update stripe.entity.ts

--- server/src/stripe/entities/stripe.entity.ts
+++ server/src/stripe/entities/stripe.entity.ts
@@ -1,1 +1,13 @@
-export class Stripe {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from 'src/config/base.entity';
+@Entity({ name: 'subscrition' })
+export class SubscriptionEntity extends BaseEntity {
+  @Column()
+  stripeSubscriptionId: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  planId: string;
+}
