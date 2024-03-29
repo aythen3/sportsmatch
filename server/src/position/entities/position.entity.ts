9d9f7b0b25300f52d61ff54e9308254b65be0aa8
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-03-29 08:11:17 -0300

    Update position.entity.ts

--- server/src/position/entities/position.entity.ts
+++ server/src/position/entities/position.entity.ts
@@ -3,7 +3,7 @@ import { BaseEntity } from 'src/config/base.entity';
 import { OfferEntity } from 'src/offer/entities/offer.entity';
 import { SportEntity } from 'src/sport/entities/sport.entity';
 import { SportmanEntity } from 'src/sportman/entities/sportman.entity';
-import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, OneToOne } from 'typeorm';
 
 @Entity({ name: 'position' })
 export class PositionEntity extends BaseEntity {
@@ -13,8 +13,8 @@ export class PositionEntity extends BaseEntity {
   @ManyToOne(() => ClubEntity, (club) => club.positions, { nullable: true })
   club?: ClubEntity;
 
-  @ManyToOne(() => OfferEntity, (offer) => offer.position, { nullable: true })
-  offer: OfferEntity;
+  @OneToMany(() => OfferEntity, (offer) => offer.position, { nullable: true })
+  offer: OfferEntity[];
 
   @OneToOne(() => SportmanEntity, { nullable: true })
   sportmen: SportmanEntity;
