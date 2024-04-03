94bbba4a2ed1a60e4f66017c8e7fcef1d2fc7a72
Mauricio <madegasperis@gmail.com>
2024-04-03 08:51:36 -0300

    fix: pull

--- client/utils/apiBackend.js
+++ client/utils/apiBackend.js
@@ -5,4 +5,6 @@ const axiosInstance = axios.create({
   baseURL: 'http://192.168.1.47:3000/api/'
 })
 
+// axiosInstance.defaults.headers.common['user_token'] = token;
+
 export default axiosInstance
