d447905f1fe9e2ecf807b6a3a1330fe2d8436013
azul <azschiaffino@gmail.com>
2024-08-17 03:13:44 -0300

    fix

--- server/src/send-mail/send-mail.module.ts
+++ server/src/send-mail/send-mail.module.ts
@@ -10,24 +10,24 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ResetCodeService } from './reset-code.service';
 import { UserService } from 'src/user/user.service';
 import { AuthJwtService } from 'src/auth-jwt/auth-jwt.service';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
-const configService = new ConfigService();
+// const configService = new ConfigService();
 
 @Module({
   imports: [
     MailerModule.forRoot({
       transport: {
-        host: configService.get('MAIL_HOST'), // Servidor SMTP
+        host: 'smtp.gmail.com', // Servidor SMTP
         port: 587,
         secure: false, // true para TLS; false para otros protocolos
         auth: {
-          user: configService.get('MAIL_USER'), // Correo electrónico de origen
-          pass: configService.get('MAIL_PASS') // Contraseña del correo electrónico de origen
+          user: 'azschiaffino@gmail.com', // Correo electrónico de origen
+          pass: 'ccuk lafv fpmh bijv' // Contraseña del correo electrónico de origen
         }
       },
       defaults: {
-        from: configService.get('FROM') // Dirección de correo electrónico del remitente
+        from: 'sportsmatch.digital@app.com' // Dirección de correo electrónico del remitente
       },
       template: {
         dir: join(__dirname, '..', 'template'), // Directorio de plantillas de correo electrónico
